0573b2228591ead9722f8dc0271f49bbad51b156
Matt Carroll <mwcarroll@gmail.com>
2020-12-02 13:18:53 -0500

    day 1 and 2 alternates

---
+++ 2020/Day02_alt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode_Alternate
+{
+    class Day02_alt
+    {
+
+        public static void Run(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"data\day02.txt");
+
+            IEnumerable<string[]> passwords = lines.ToList().Select(x =>
+            {
+                return Regex.Split(x.Trim(), @" |-|: ");
+            });
+
+            Console.WriteLine("Day 2:");
+            Console.WriteLine("\tPart A: {0}", passwords.Where(x => x[3].Count(y => y == x[2].ToCharArray()[0]) >= int.Parse(x[0]) && x[3].Count(y => y == x[2].ToCharArray()[0]) <= int.Parse(x[1])).Count());
+            Console.WriteLine("\tPart B: {0}", passwords.Where(x => x[3][int.Parse(x[0]) - 1] == x[2].ToCharArray()[0] ^ x[3][int.Parse(x[1]) - 1] == x[2].ToCharArray()[0]).Count());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ https://adventofcode.com/2020
 * [Day 1: Report Repair](2020/Day01.cs)
   * [Alternate Solution](2020/Day01_alt.cs)
 * [Day 2: Password Philosophy](2020/Day02.cs)
+  * [Alternate Solution](2020/Day02_alt.cs)
